729a317cd51301098ff8bbcbc328a0cfdd66a9fc
Lucas <lrodriguez@sundes.com>
2025-05-27 10:00:10 -0400

    Add index.ts for top-level component exports

---
+++ index.ts
@@ -0,0 +1,32 @@
+// UI Components exports
+export * from "./src/components/ui/accordion";
+export * from "./src/components/ui/avatar";
+export * from "./src/components/ui/badge";
+export * from "./src/components/ui/button";
+export * from "./src/components/ui/calendar";
+export * from "./src/components/ui/card";
+export * from "./src/components/ui/checkbox";
+export * from "./src/components/ui/comboBox";
+export * from "./src/components/ui/command";
+export * from "./src/components/ui/dayPicker";
+export * from "./src/components/ui/dialog";
+export * from "./src/components/ui/dropdown-menu";
+export * from "./src/components/ui/horizontalDayPicker";
+export * from "./src/components/ui/input";
+export * from "./src/components/ui/label";
+export * from "./src/components/ui/multiselect";
+export * from "./src/components/ui/multiStepper";
+export * from "./src/components/ui/phone-input";
+export * from "./src/components/ui/popover";
+export * from "./src/components/ui/scroll-area";
+export * from "./src/components/ui/select";
+export * from "./src/components/ui/separator";
+export * from "./src/components/ui/switch";
+export * from "./src/components/ui/tabs";
+export * from "./src/components/ui/toggle";
+
+// Special components
+export * from "./src/components/ChileanRutInput";
+
+// Utils
+export * from "./src/lib/utils";
